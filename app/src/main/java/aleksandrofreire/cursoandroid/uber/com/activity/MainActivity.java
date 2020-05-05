fe5702685bad7ed1e41206f6c0735648b213e4bc
alekscosmic <64270357+alekscosmic@users.noreply.github.com>
2020-05-04 21:23:08 -0300

    Aula 393 Criando interface de autenticação e efeito botão

---
+++ app/src/main/java/aleksandrofreire/cursoandroid/uber/com/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package aleksandrofreire.cursoandroid.uber.com.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import aleksandrofreire.cursoandroid.uber.com.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal);
+
+        getSupportActionBar().hide();
+    }
+}
